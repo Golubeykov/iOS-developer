acd5ac11ac7e5fb8bb16151298a7508cbf5a3281
GolubeykovAnton <ds6580@yandex.ru>
2019-02-06 21:59:21 +0300

    HW 3

---
+++ HW_3.playground/Contents.swift
@@ -0,0 +1,75 @@
+import Cocoa
+import Foundation
+
+enum door {
+    case opened
+    case closed
+}
+
+struct Car {
+    var brand : String
+    var year : Int
+    var trunk : Double
+    var door : door
+    var trunkVused : Double
+    var isEngine : Bool
+    var isWindows : Bool
+    
+    var info : String {
+        get {
+            return brand + " " + (String(year))
+        }
+    }
+    
+    mutating func turnOnOffEngine () {
+        isEngine = !isEngine
+    }
+    
+    mutating func OpenCloseWindow () {
+        isWindows = !isWindows
+    }
+    
+    mutating func UsedVTrunkChange () {
+        trunkVused = 100
+    }
+}
+
+
+struct Truck {
+    var brand : String
+    var year : Int
+    var trunk : Double
+    var door : door
+    var trunkVused : Double
+    var isEngine : Bool
+    var isWindows : Bool
+    
+    mutating func turnOnOffEngine () {
+        isEngine = !isEngine
+    }
+    
+    mutating func OpenCloseWindow () {
+        isWindows = !isWindows
+    }
+    
+    mutating func UsedVTrunkChange() {
+    trunkVused = 100
+    }
+    
+}
+
+
+var car1 = Car (brand: "BMW", year: 2012, trunk: 550.0, door: .opened, trunkVused: 200.0, isEngine: true, isWindows: true)
+var car2 = Car (brand: "KIA", year: 2015, trunk: 250.0, door: .opened, trunkVused: 200.0, isEngine: false, isWindows: true)
+print(car1.info)
+
+
+var truck1 = Truck (brand: "MAZ", year: 2012, trunk: 2550.0, door: .closed, trunkVused: 2000.0, isEngine: true, isWindows: false)
+var truck2 = Truck (brand: "KAMAZ", year: 2015, trunk: 2250.0, door: .opened, trunkVused: 2000.0, isEngine: false, isWindows: false)
+
+car1.OpenCloseWindow()
+truck2.UsedVTrunkChange()
+print(car1.isWindows)
+print(truck2.trunkVused)
+
+
