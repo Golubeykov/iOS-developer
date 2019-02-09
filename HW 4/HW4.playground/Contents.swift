8a2fda85baa4f8ecc72f9cfe4e003afb30824b30
GolubeykovAnton <ds6580@yandex.ru>
2019-02-09 18:35:11 +0300

    Сделал ДЗ4

---
+++ HW 4/HW4.playground/Contents.swift
@@ -0,0 +1,97 @@
+import Cocoa
+import Foundation
+
+enum DoorState {
+    case open, close
+}
+
+enum Transmission {
+    case manual, auto
+}
+
+enum Hatch {
+    case open, close
+}
+
+enum TrunkOperation {
+    case load, unload
+}
+
+class Car {
+    let color: String
+    let mp3: Bool
+    let transmission: Transmission
+    var km: Double
+    var doorState: DoorState
+    var trunk : Double
+    var trunkCapacity : Double
+    func carsound () {
+        print("Car sound")
+    }
+    func TrunkChange(operation : TrunkOperation, cargo: Double) -> Bool {
+        switch operation {
+        case .load:
+            if (self.trunkCapacity < (self.trunk + cargo)) == false {
+                self.trunk = self.trunk + cargo
+                return true
+            } else {
+                print("Нет места в багажнике")
+                return false
+            }
+        case .unload:
+            if ((trunk-cargo) < 0) == true {
+                print("В багажнике меньше груза, чем вы пытаетесь выгрузить")
+                return false
+            } else {
+                trunk = trunk - cargo
+                return true
+            }
+        }
+
+    }
+    
+    init(color: String, mp3: Bool, transmission: Transmission, km: Double, doorState: DoorState, trunk : Double, trunkCapacity : Double) {
+        self.color = color
+        self.mp3 = mp3
+        self.transmission = transmission
+        self.km = km
+        self.doorState = doorState
+        self.trunk = trunk
+        self.trunkCapacity = trunkCapacity
+    }
+}
+
+class SportCar: Car {
+    var hatch : Hatch
+    init (color: String, mp3: Bool, transmission: Transmission, km: Double, doorState: DoorState, hatch : Hatch, trunk : Double, trunkCapacity : Double) {
+        self.hatch = hatch
+        super.init(color: color, mp3: mp3, transmission: transmission, km: km, doorState: doorState, trunk: trunk, trunkCapacity: trunkCapacity)
+    }
+    override func carsound() {
+        print("Rrrrr")
+    }
+}
+
+class AvtoVAZ: Car {
+    var PutinInside : Bool
+    var WorkingEngine : Bool
+    init(color: String, mp3: Bool, transmission: Transmission, km: Double, doorState: DoorState, PutinInside : Bool, WorkingEngine : Bool, trunk : Double, trunkCapacity : Double) {
+        self.PutinInside = PutinInside
+        self.WorkingEngine = WorkingEngine
+        super.init(color: color, mp3: mp3, transmission: transmission, km: km, doorState: doorState, trunk: trunk, trunkCapacity: trunkCapacity)
+    }
+    override func carsound() {
+        print("Pfff")
+    }
+}
+
+var porsche : SportCar = SportCar (color: "Red", mp3: true, transmission: .auto, km: 1000, doorState: .close, hatch: .open, trunk: 10, trunkCapacity: 400)
+
+var ladaKalina : AvtoVAZ = AvtoVAZ (color: "yellow", mp3: false, transmission: .manual, km: 4000, doorState: .open, PutinInside: true, WorkingEngine: false, trunk: 300, trunkCapacity: 600)
+
+ladaKalina.TrunkChange(operation: .load, cargo: 200)
+print("Текущая загруженность багажника", (ladaKalina.trunk))
+
+ladaKalina.carsound()
+porsche.carsound()
+
--- HW_3.playground/Contents.swift
+++ HW_3.playground/Contents.swift
@@ -1,7 +1,7 @@
 import Cocoa
 import Foundation
 
-enum door {
+enum doorstate {
     case opened
     case closed
 }
@@ -10,7 +10,7 @@ struct Car {
     var brand : String
     var year : Int
     var trunk : Double
-    var door : door
+    var door : doorstate
     var trunkVused : Double
     var isEngine : Bool
     var isWindows : Bool
@@ -18,6 +18,14 @@ struct Car {
     var info : String {
         get {
             return brand + " " + (String(year))
+        }
+    }
+    mutating func OpenCloseDoorCar (state : doorstate) {
+        switch state {
+        case .opened:
+            print ("Дверь открывается")
+        case .closed:
+            print("Дверь закрывается")
         }
     }
     
@@ -39,7 +47,7 @@ struct Truck {
     var brand : String
     var year : Int
     var trunk : Double
-    var door : door
+    var door : doorstate
     var trunkVused : Double
     var isEngine : Bool
     var isWindows : Bool
@@ -67,8 +75,10 @@ print(car1.info)
 var truck1 = Truck (brand: "MAZ", year: 2012, trunk: 2550.0, door: .closed, trunkVused: 2000.0, isEngine: true, isWindows: false)
 var truck2 = Truck (brand: "KAMAZ", year: 2015, trunk: 2250.0, door: .opened, trunkVused: 2000.0, isEngine: false, isWindows: false)
 
+car1.OpenCloseDoorCar(state: .closed)
 car1.OpenCloseWindow()
 truck2.UsedVTrunkChange()
+
 print(car1.isWindows)
 print(truck2.trunkVused)
 
